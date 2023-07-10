4525cc048cdd852af7e2cd70dab131ce2d841c3e
victoMR <ratonrangelmejia+2@gmail.com>
2023-07-09 23:56:48 -0600

    Co-authored-by: doloresabrilsanchez@gmail.com <doloresabrilsanchez@gmail.com>

--- index.js
+++ index.js
@@ -4,27 +4,37 @@ var session= require("express-session");
 var usrRuta= require("./rutas/usuarios");
 require("dotenv").config();
 
-
-
 var app = express();
 app.set('view engine', 'ejs');
 app.use("/web", express.static(path.join(__dirname,"/web")));
 app.use(express.urlencoded({ extended:true }));
-
 app.use(express.json());
-
 app.use(session({
-    secret:process.env.SECRETO_SESSION,
-    resave:true,
-    saveUninitialized:true
+    secret: process.env.SECRETO_SESSION,
+    resave: true,
+    saveUninitialized: true
 }));
 
-app.use("/",usrRuta);
-  
+function findAvailablePort(ports, callback) {
+  if (ports.length === 0) {
+    throw new Error('No available ports found');
+  }
 
-var port=process.env.PORT || 3000;
+  var port = ports.shift();
+  app.listen(port, function(error) {
+    if (error && error.code === 'EADDRINUSE') {
+      findAvailablePort(ports, callback);
+    } else {
+      callback(port);
+    }
+  });
+}
 
-app.listen(port,() => {
-    console.log(`Servidor en http://localhost:${port}`);
+var portSequence = [8000, 8001, 8002, 8003, 8004, 8005];
+
+findAvailablePort(portSequence, function(port) {
+  console.log(`Servidor en http://localhost:${port}`);
 });
+
+app.use("/", usrRuta);
 
--- web/js/almaguer.js
+++ web/js/almaguer.js
@@ -1,4 +1,4 @@
-// Inicia script del cuadro
+
 const gridItems = document.querySelectorAll(".grid-item");
 
 gridItems.forEach(function(item) {
@@ -45,69 +45,15 @@ closeButton.addEventListener("click", function() {
   popup.style.display = "none";
 });
 
-const popupButton = document.getElementById("popup-button");
+var popupButton = document.getElementById("popup-button");
 
-// Define una variable global para almacenar los elementos del carrito
-let cartItems = [];
 
-// Función para agregar un elemento al carrito
-function addToCart(product) {
-  // Agrega el producto al arreglo de elementos del carrito
-  cartItems.push(product);
+let increment = (popupButton) =>{
+  console.log(popupButton);
+};
 
-  // Actualiza el contador del carrito
-  updateCartCount();
+let decrement = () =>{
+  console.log("Quitar producto");
+};
+let update = () =>{};
 
-  // Guarda los elementos del carrito en localStorage
-  saveCartToLocalStorage();
-}
-
-// Función para actualizar el contador del carrito
-function updateCartCount() {
-  const cartCount = document.querySelector(".cartAmount");
-  cartCount.textContent = cartItems.length;
-}
-
-// Obtén todos los botones "Agregar al carrito" del popup
-const addToCartButtons = document.querySelectorAll("#popup-button");
-
-// Agrega un controlador de eventos a cada botón
-addToCartButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    // Obtén los detalles del producto desde el popup
-    const popupTitle = document.getElementById("popup-title").textContent;
-    const popupImage = document.getElementById("popup-image").style.backgroundImage;
-    const popupDescription = document.getElementById("popup-description").textContent;
-    const popupIngredients = Array.from(document.querySelectorAll("#popup-ingredients li"))
-      .map((ingredient) => ingredient.textContent);
-
-    // Crea un objeto de producto
-    const product = {
-      title: popupTitle,
-      image: popupImage,
-      description: popupDescription,
-      ingredients: popupIngredients
-    };
-
-    // Agrega el producto al carrito
-    addToCart(product);
-  });
-});
-
-// Función para guardar los elementos del carrito en localStorage
-function saveCartToLocalStorage() {
-  localStorage.setItem("cartItems", JSON.stringify(cartItems));
-}
-
-// Función para cargar los elementos del carrito desde localStorage
-function loadCartFromLocalStorage() {
-  const cartItemsJson = localStorage.getItem("cartItems");
-  if (cartItemsJson) {
-    cartItems = JSON.parse(cartItemsJson);
-    // Actualiza el contador del carrito al cargar la página
-    updateCartCount();
-  }
-}
-
-// Llama a la función para cargar los elementos del carrito al cargar la página
-loadCartFromLocalStorage();
